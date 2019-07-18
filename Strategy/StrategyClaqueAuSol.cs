487d0cddace598a4b6d2fe9be3bf62d3197bb9fd
Valentin Nasraty <valentin.nasraty@epitech.eu>
2019-07-18 20:27:57 +0200

    EnsureProfit fonctionne par Pourcentage
    BuyEnsureProfit & SellEnsureProfit devienne BuyPercentageEnsureProfit & SellPercentageEnsureProfit

--- Strategy/StrategyClaqueAuSol.cs
+++ Strategy/StrategyClaqueAuSol.cs
@@ -18,35 +18,36 @@ namespace NinjaTrader.Strategy
     /// <summary>
     /// 
     /// </summary>
-    [Description("Une strategie claqué au sol - v1")]
+    [Description("Une strategie claqué au sol - v1.1")]
     public class StrategyClaqueAuSol : Strategy
     {
         #region Variables
         // Wizard generated variables
-        private int sellThreshold = 25000; // Default setting for SellThreshold
-		private int sellLimitOrder = 0; // Default setting for SellLimitOrder
-        private int sellTrailStop = 0; // Default setting for SellTrailStop
-        private int sellTakeProfit = 10000; // Default setting for SellTakeProfit
-		private double sellEnsureProfit = 1000; // Default setting for SellEnsureProfit
-		
-        private int buyThreshold = -25000; // Default setting for BuyThreshold
-		private int buyLimitOrder = 0; // Default setting for LuyLimitOrder
-        private int buyTrailStop = 0; // Default setting for BuyTrailStop
-        private int buyTakeProfit = -10000; // Default setting for BuyTakeProfit
-		private double buyEnsureProfit = 1000; // Default setting for BuyEnsureProfit
-		
+        private int sellThreshold = 30; // Default setting for SellThreshold
+        private int sellLimitOrder = 0; // Default setting for SellLimitOrder
+        private int sellTrailStop = 500; // Default setting for SellTrailStop
+        private int sellTakeProfit = -100; // Default setting for SellTakeProfit
+        private double sellPercentageEnsureProfit = 50; // Default setting for SellPercentageEnsureProfit
+
+        private int buyThreshold = -30; // Default setting for BuyThreshold
+        private int buyLimitOrder = 0; // Default setting for LuyLimitOrder
+        private int buyTrailStop = 500; // Default setting for BuyTrailStop
+        private int buyTakeProfit = 100; // Default setting for BuyTakeProfit
+        private double buyPercentageEnsureProfit = 50; // Default setting for BuyPercentageEnsureProfit
+
         // User defined variables (add any user defined variables below)
-		private double barSize;
-		private double emaSize;
-		private double emaOpenSize;
+        private double barSize;
+        private double emaSize;
+        private double emaOpenSize;
         private bool offerPlaced;
-		private bool isOnBuy;
-		private bool isOnSell;
-		private bool asMadeProfit;
-		private double bestProfitBuy;
-		private double bestProfitSell;
-		private double buyEntryPrice;
-		private double sellEntryPrice;
+        private bool isOnBuy;
+        private bool isOnSell;
+        private bool asMadeProfit;
+        private double flagProfit;
+        private double bestEmaBuy;
+        private double bestEmaSell;
+        private double buyEntryPrice;
+        private double sellEntryPrice;
         #endregion
 
         /// <summary>
@@ -57,137 +58,150 @@ namespace NinjaTrader.Strategy
             offerPlaced = false;
         }
 
-		private void initOnBarUpdate()
-		{
-			emaSize = EMA(0)[0];
-			emaOpenSize = Open[0];
-			barSize = ((Input[0] - emaOpenSize) / TickSize);
-		}
-		
-		private void initAfterOfferClosed()
-		{
-			isOnBuy = false;
-			isOnSell = false;
-			asMadeProfit = false;
-			bestProfitBuy = Double.MinValue;
-			bestProfitSell = Double.MaxValue;
-		}
-		
+        private void initOnBarUpdate()
+        {
+            emaSize = EMA(0)[0];
+            emaOpenSize = Open[0];
+            barSize = ((Input[0] - emaOpenSize) / TickSize);
+        }
+
+        private void initAfterOfferClosed()
+        {
+            isOnBuy = false;
+            isOnSell = false;
+            asMadeProfit = false;
+            flagProfit = 0;
+            bestEmaBuy = Double.MinValue;
+            bestEmaSell = Double.MaxValue;
+        }
+
         /// <summary>
         /// Called on each bar update event (incoming tick)
         /// </summary>
         protected override void OnBarUpdate()
         {
-			if (Historical) 
-				return;
-			initOnBarUpdate();
-			if (!offerPlaced)
-			{
-				initAfterOfferClosed();
-				checkToPlaceOffer();
-			}
-			else
-				checkProfit();
-			// lorsqu'il créer un nouveau bloc, il recommence a chercher a buy ou sell
-			/*if (FirstTickOfBar)
+            if (Historical)
+                return;
+            initOnBarUpdate();
+            if (!offerPlaced)
+            {
+                initAfterOfferClosed();
+                checkToPlaceOffer();
+            }
+            else
+                checkProfit();
+            // lorsqu'il créer un nouveau bloc, il recommence a chercher a buy ou sell
+            /*if (FirstTickOfBar)
 				offerPlaced = false;*/
         }
-		
-		private void checkToPlaceOffer()
-		{
-			if (shouldBuy())
-				handleBuying();
-			else if (shouldSell())
-				handleSelling();
-		}
-		
-		private void handleBuying()
-		{
+
+        private void checkToPlaceOffer()
+        {
+            if (shouldBuy())
+                handleBuying();
+            else if (shouldSell())
+                handleSelling();
+        }
+
+        private void handleBuying()
+        {
             double trailStop = (buyTrailStop * TickSize);
-			double takeProfitValue = (emaOpenSize + (buyTakeProfit * TickSize));
-			buyEntryPrice = (emaOpenSize + (buyLimitOrder * TickSize));
-			
-			EnterLongStop(buyEntryPrice); // achete lorsque entryPrice est atteint
-			SetTrailStop(CalculationMode.Ticks, buyTrailStop); // exit l'action,
-			SetProfitTarget(CalculationMode.Price, takeProfitValue); // exit l'action,
-			offerPlaced = true;
-			isOnBuy = true;
-		}
-		
-		private void handleSelling() 
-		{
+            double takeProfitValue = (emaOpenSize + (buyTakeProfit * TickSize));
+            buyEntryPrice = (emaOpenSize + (buyLimitOrder * TickSize));
+
+            EnterLongStop(buyEntryPrice); // achete lorsque entryPrice est atteint
+            SetTrailStop(CalculationMode.Ticks, buyTrailStop); // exit l'action,
+            SetProfitTarget(CalculationMode.Price, takeProfitValue); // exit l'action,
+            offerPlaced = true;
+            isOnBuy = true;
+        }
+
+        private void handleSelling()
+        {
             double trailStop = (sellTrailStop * TickSize);
-			double takeProfitValue = (emaOpenSize + (sellTakeProfit * TickSize));
-			sellEntryPrice = (emaOpenSize + (sellLimitOrder * TickSize));
-			
-			EnterShortStop(sellEntryPrice);
-			SetTrailStop(CalculationMode.Ticks, sellTrailStop);  /// voir stop loss
+            double takeProfitValue = (emaOpenSize + (sellTakeProfit * TickSize));
+            sellEntryPrice = (emaOpenSize + (sellLimitOrder * TickSize));
+
+            EnterShortStop(sellEntryPrice);
+            SetTrailStop(CalculationMode.Ticks, sellTrailStop);  /// voir stop loss
 			SetProfitTarget(CalculationMode.Price, takeProfitValue);
-			offerPlaced = true;
-			isOnSell = true;
-		}
-		
-		private void checkProfit() 
-		{
-			if (isOnBuy)
-				checkBuyProfit();
-			else if (isOnSell)
-				checkSellProfit();
-		}
-		
-		private void checkBuyProfit()
-		{
-			double tmpProfit = emaSize;
-			double triggerEnsureProfit = (bestProfitBuy - (buyEnsureProfit * TickSize));
-			
-			if (isMakingProfitBuy()) 
-			{
-				if (tmpProfit >= bestProfitBuy) // on recupere le plus haut profit
-					bestProfitBuy = tmpProfit;
-			}
-			if (tmpProfit <= triggerEnsureProfit) // on stop si on atteint le trigger EnsureProfit
-			{
-				ExitLong();
-				offerPlaced = false;
-			}
-		}
-		
-		private void checkSellProfit()
-		{
-			double tmpProfit = emaSize;
-			double triggerEnsureProfit = (bestProfitSell + (sellEnsureProfit * TickSize));
-			
-			if (isMakingProfitSell()) 
-			{
-				if (tmpProfit <= bestProfitSell)
-					bestProfitSell = tmpProfit;
-			}
-			if (tmpProfit >= triggerEnsureProfit)
-			{
-				ExitShort();
-				offerPlaced = false;
-			}
-		}
-		
-		private bool shouldBuy()
-		{
-			return (barSize <= buyThreshold);
-		}
-		
-		private bool shouldSell() 
-		{
-			return (barSize >= sellThreshold);
-		}
-		
-		private bool isMakingProfitBuy() 
-		{
-			return (emaSize > buyEntryPrice);
-		}
-		
-		private bool isMakingProfitSell() 
-		{
-			return (emaSize < sellEntryPrice);
-		}
+            offerPlaced = true;
+            isOnSell = true;
+        }
+
+        private void checkProfit()
+        {
+            if (isOnBuy)
+                checkBuyProfit();
+            else if (isOnSell)
+                checkSellProfit();
+        }
+
+        private void checkBuyProfit()
+        {
+            double maxProfit = (bestEmaBuy - flagProfit); // calcul le profit actuel
+            double percentageEnsureProfit = (maxProfit * (buyPercentageEnsureProfit * 0.01));
+            double triggerEnsureProfit = (bestEmaBuy - percentageEnsureProfit);
+
+            if (isMakingProfitBuy())
+            {
+                if (!asMadeProfit) // on recupere la valeur de EMA au moment ou il fait du profit pour la 1ere fois
+                {
+                    flagProfit = emaSize;
+                    asMadeProfit = true;
+                }
+                if (emaSize >= bestEmaBuy) // on recupere le plus haut EMA
+                    bestEmaBuy = emaSize;
+            }
+            if (emaSize < triggerEnsureProfit) // on stop si on passe sous le trigger EnsureProfit
+            {
+                ExitLong();
+                offerPlaced = false;
+            }
+        }
+
+        private void checkSellProfit()
+        {
+            double maxProfit = (flagProfit - bestEmaSell);
+            double percentageEnsureProfit = (maxProfit * (sellPercentageEnsureProfit * 0.01));
+            double triggerEnsureProfit = (bestEmaSell + percentageEnsureProfit);
+
+            if (isMakingProfitSell())
+            {
+                if (!asMadeProfit)
+                {
+                    flagProfit = emaSize;
+                    asMadeProfit = true;
+                }
+                if (emaSize <= bestEmaSell)
+                    bestEmaSell = emaSize;
+            }
+            if (emaSize > triggerEnsureProfit)
+            {
+                ExitShort();
+                offerPlaced = false;
+            }
+        }
+
+        private bool shouldBuy()
+        {
+            return (barSize <= buyThreshold);
+        }
+
+        private bool shouldSell()
+        {
+            return (barSize >= sellThreshold);
+        }
+
+        private bool isMakingProfitBuy()
+        {
+            return (emaSize > buyEntryPrice);
+        }
+
+        private bool isMakingProfitSell()
+        {
+            return (emaSize < sellEntryPrice);
+        }
 
         #region Properties
         [Description("Trigger qui place un ordre à une valeur donné, si il est atteint.")]
@@ -197,7 +211,7 @@ namespace NinjaTrader.Strategy
             get { return sellThreshold; }
             set { sellThreshold = value; }
         }
-		
+
         [Description("Valeur de l'ordre lorque le trigger Threshold est déclenché.")]
         [GridCategory("Parameters")]
         public int SellLimitOrder
@@ -221,15 +235,15 @@ namespace NinjaTrader.Strategy
             get { return sellTakeProfit; }
             set { sellTakeProfit = value; }
         }
-		
-		[Description("Trigger qui close un ordre lorque sa valeur est atteinte. Il permet de sécuriser le bénéfice ou d'empecher le déficit. Lorsque l'on fait un bénéfice, si l'on se met à perdre il se déclenche à la valeur donnée.")]
+
+        [Description("Trigger, sous forme de pourcentage, qui close un ordre lorque sa valeur est atteinte. Il permet de sécuriser le bénéfice ou d'empecher le déficit. Lorsque l'on fait un bénéfice, si l'on se met à perdre il se déclenche à la valeur donnée.")]
         [GridCategory("Parameters")]
-        public double SellEnsureProfit
+        public double SellPercentageEnsureProfit
         {
-            get { return sellEnsureProfit; }
-            set { sellEnsureProfit = value; }
+            get { return sellPercentageEnsureProfit; }
+            set { sellPercentageEnsureProfit = value; }
         }
-		
+
         [Description("Trigger qui place un ordre à une valeur donné, si il est atteint.")]
         [GridCategory("Parameters")]
         public int BuyThreshold
@@ -261,13 +275,13 @@ namespace NinjaTrader.Strategy
             get { return buyTakeProfit; }
             set { buyTakeProfit = value; }
         }
-		
-		[Description("Trigger qui close un ordre lorque sa valeur est atteinte. Il permet de sécuriser le bénéfice ou d'empecher le déficit. Lorsque l'on fait un bénéfice, si l'on se met à perdre il se déclenche à la valeur donnée.")]
+
+        [Description("Trigger, sous forme de pourcentage, qui close un ordre lorque sa valeur est atteinte. Il permet de sécuriser le bénéfice ou d'empecher le déficit. Lorsque l'on fait un bénéfice, si l'on se met à perdre il se déclenche à la valeur donnée.")]
         [GridCategory("Parameters")]
-        public double BuyEnsureProfit
+        public double BuyPercentageEnsureProfit
         {
-            get { return buyEnsureProfit; }
-            set { buyEnsureProfit = value; }
+            get { return buyPercentageEnsureProfit; }
+            set { buyPercentageEnsureProfit = value; }
         }
         #endregion
     }
